f37f5fff98fcb21183deded00f3dd3c9f9cd2465
LedAndris <ledererandras2004@gmail.com>
2021-03-23 00:56:00 +0100

    feat(client/core): Added car command

--- client/client.ts
+++ client/client.ts
@@ -1,3 +1,12 @@
+import * as Cfx from 'fivem-js';
+
+
 RegisterCommand("warpIsland", async (source: string, args: string[]) => {
-  emit("TXPVP:CORE:loadIsland")
-}, true)
+  emit("TXPVP:CORE:loadIsland");
+}, true);
+
+
+RegisterCommand('car', async (source: number, args: string[]) => {
+  const vehicle = await Cfx.World.createVehicle(new Cfx.Model(args[0]), Cfx.Game.PlayerPed.Position, Cfx.Game.PlayerPed.Heading);
+  Cfx.Game.PlayerPed.setIntoVehicle(vehicle, Cfx.VehicleSeat.Driver);
+}, true);
