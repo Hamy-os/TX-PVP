d1b635f6e3bc819831fda27bfaf621e017e97954
LedAndris <ledererandras2004@gmail.com>
2021-03-23 16:20:33 +0100

    removed type too

--- client/typings.ts
+++ client/typings.ts
@@ -1,7 +1,3 @@
 export type WeaponString = string
 export type Loadout = { DEA: {[key: string]: WeaponString[] }, NARCO: {[key: string]: WeaponString[]} }
 export type Team = "DEA" | "NARCO"
-
-export interface Map<T> {
-  [key: string]: T
-}
