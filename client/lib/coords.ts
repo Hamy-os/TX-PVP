d019e70c586e113add9815702ac302cc0e68c58a
Hamy-os <hamzaalisic0@gmail.com>
2021-04-03 14:59:39 +0300

    tweak(Main/commands) Changed commands to lower case and added chat suggestions

--- client/lib/coords.ts
+++ client/lib/coords.ts
@@ -4,17 +4,17 @@ import { castVec3 } from "./";
 import { getCams } from "./json";
 import { Blip } from "./classes/blip";
 
-spwn.set("airField", new Vector3(4504.017578125, -4534.1342773438, 4.1950516700745))
-spwn.set("lobbySpawn", new Vector3(121.1, -728.64, 242.15))
-spwn.set("smallPort", new Vector3(4984.4990234375, -5163.41015625, 2.5144073963165))
+spwn.set("airfield", new Vector3(4504.017578125, -4534.1342773438, 4.1950516700745))
+spwn.set("lobbyspawn", new Vector3(121.1, -728.64, 242.15))
+spwn.set("smallport", new Vector3(4984.4990234375, -5163.41015625, 2.5144073963165))
 spwn.set("mansion", new Vector3(4977.6723632812, -5598.3408203125, 23.836664199829))
-spwn.set("bigPort", new Vector3(5062.8637695312, -4622.4125976562, 2.6441102027893))
-spwn.set("wareHouses", new Vector3(5158.2358398438, -5224.2934570312, 7.1033706665039))
-spwn.set("partyPlace", new Vector3(4900.06640625, -4917.12109375, 3.3773925304413))
-spwn.set("northYankton", new Vector3(3217.697, 	-4834.826, 111.8152))
-spwn.set("dinghyTest", new Vector3(5103.961, -5115.153, 0.3953705))
+spwn.set("bigport", new Vector3(5062.8637695312, -4622.4125976562, 2.6441102027893))
+spwn.set("warehouses", new Vector3(5158.2358398438, -5224.2934570312, 7.1033706665039))
+spwn.set("partyplace", new Vector3(4900.06640625, -4917.12109375, 3.3773925304413))
+spwn.set("northyankton", new Vector3(3217.697, 	-4834.826, 111.8152))
+spwn.set("dinghytest", new Vector3(5103.961, -5115.153, 0.3953705))
 const cams: { coords: Vector3, rot: number, name: string }[] = [
-  { coords: new Vector3(4515.3837890625, -4529.1918945312, 7.1206555366516), rot: 19.186916351318, name: "Airfield" },
+  { coords: new Vector3(4515.3837890625, -4529.1918945312, 7.1206555366516), rot: 19.186916351318, name: "airfield" },
   { coords: new Vector3(5267.435546875, -5425.7543945312, 161.04574584961), rot: -20.520044326782, name: "Relay twr" },
   { coords: new Vector3(5107.8071289062, -4585.2094726562, 27.717777252197), rot: 189.19686889648, name: "Port 1" },
   { coords: new Vector3(5041.2368164062, -5117.5063476562, 24.944631576538), rot: 101.55718994141, name: "Port 2" },
--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,9 @@ async function main() {
   ServerCallback.listen()
   Manager.listen()
   console.log("Running")
-  RegisterCommand("addPlayerToTeam", (source: string, args: string[]) => {
+
+  //Join a team usage: /jointeam Name
+  RegisterCommand("jointeam", (source: string, args: string[]) => {
     const src = source
     console.log("Adding player to team", src, args[0])
     addPlayerToTeam(src, args[0] as Team)
